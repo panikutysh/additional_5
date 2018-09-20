9df7614bb7e0744f7314bf97cc13077a8ff81325
panikutysh <pani.kutysh@gmail.com>
2018-09-20 15:27:02 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var StackOpen = [];
+  correct = true;
+  //перебираем строку
+  for (let index = 0; index < str.length; index++) {
+    var open = false;
+    const element = str[index];
+      for (let BCindex = 0; BCindex < bracketsConfig.length; BCindex++) {
+        const BCelement = bracketsConfig[BCindex];
+        //проверяем является ли текущий элемент строки открывающей скобкой
+        if (element == BCelement[0])
+        {
+          open = true;
+          //если элемент открывающий - добавляем в стек
+          StackOpen.push(element);
+        }
+      }
+    //если элемент закрывающий  
+    if (open==false)
+    {
+      //ищем в каком подмассиве описан этот тип скобок
+      for (let BCindex = 0; BCindex < bracketsConfig.length; BCindex++) {
+        const BCelement = bracketsConfig[BCindex];
+        if (element == BCelement[1])
+        { //читаем последний элемент стека
+          LastInElement = StackOpen.pop(); 
+          //проверяем соответствует ли последний элемент стека открывающей скобке 
+          if (BCelement[0] == LastInElement)
+          {
+            correct = true;
+          }
+          else
+          {
+            correct = false;
+            return correct; 
+          }
+        }
+      }
+    }  
+  }
+  if ((correct)&&(StackOpen.length==0))
+  {
+    return correct;
+  }
+  else
+  {
+    return !correct;
+  }
 }
